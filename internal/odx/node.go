5e8aa4e2e56e382d6f8aecc7cd40c541110e36be
Piero Toffanin <pt@masseranolabs.com>
2026-04-16 22:58:48 -0400

    Add missing files

---
+++ internal/odx/node.go
@@ -0,0 +1,495 @@
+// Copyright © 2026 CloudODX Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package odx
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
+	"math"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	odxio "github.com/WebODM/CloudODX/internal/io"
+	"github.com/WebODM/CloudODX/internal/logger"
+	"github.com/cheggaaa/pb"
+)
+
+// ErrUnauthorized means a response was not authorized
+var ErrUnauthorized = errors.New("Unauthorized")
+
+// ErrAuthRequired means authorization is required
+var ErrAuthRequired = errors.New("Auth Required")
+
+type InfoResponse struct {
+	Version   string `json:"version"`
+	MaxImages int    `json:"maxImages"`
+
+	Error string `json:"error"`
+}
+
+type OptionResponse struct {
+	Domain interface{} `json:"domain"`
+	Help   string      `json:"help"`
+	Name   string      `json:"name"`
+	Type   string      `json:"type"`
+	Value  string      `json:"value"`
+}
+
+type AuthInfoResponse struct {
+	Message     string `json:"message"`
+	LoginUrl    string `json:"loginUrl"`
+	RegisterUrl string `json:"registerUrl"`
+}
+
+type StatusCode struct {
+	Code int `json:"code"`
+}
+
+type TaskInfoResponse struct {
+	ProcessingTime int        `json:"processingTime"`
+	Status         StatusCode `json:"status"`
+}
+
+type ApiActionResponse struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
+}
+
+type TaskNewResponse struct {
+	UUID  string `json:"uuid"`
+	Error string `json:"error"`
+}
+
+// Node is a NodeODX processing node
+type Node struct {
+	URL   string `json:"url"`
+	Token string `json:"token"`
+
+	_debugUnauthorized bool
+}
+
+func (n Node) String() string {
+	return n.URL
+}
+
+// URLFor builds a URL path
+func (n Node) URLFor(path string) string {
+	u, err := url.ParseRequestURI(n.URL + path)
+	if err != nil {
+		return ""
+	}
+	q := u.Query()
+	if len(n.Token) > 0 {
+		q.Add("token", n.Token)
+	}
+	if n._debugUnauthorized {
+		q.Add("_debugUnauthorized", "1")
+	}
+	u.RawQuery = q.Encode()
+	return u.String()
+}
+
+func (n Node) apiGET(path string) ([]byte, error) {
+	url := n.URLFor(path)
+	logger.Debug("GET: " + url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 401 {
+		return nil, ErrUnauthorized
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New("Server returned status code: " + strconv.Itoa(resp.StatusCode))
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func (n Node) apiPOST(path string, values map[string]string) ([]byte, error) {
+	targetURL := n.URLFor(path)
+	logger.Debug("POST: " + targetURL)
+
+	formData := url.Values{}
+	for k, v := range values {
+		formData.Set(k, v)
+		logger.Debug(k + ": " + v)
+	}
+
+	resp, err := http.Post(targetURL, "application/x-www-form-urlencoded", strings.NewReader(formData.Encode()))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New("Server returned status code: " + strconv.Itoa(resp.StatusCode))
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+// Info GET: /info
+func (n Node) Info() (*InfoResponse, error) {
+	res := InfoResponse{}
+	body, err := n.apiGET("/info")
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return nil, err
+	}
+
+	if res.Error != "" {
+		if strings.HasPrefix(res.Error, "Invalid authentication token") {
+			return nil, ErrUnauthorized
+		}
+		return nil, errors.New(res.Error)
+	}
+
+	if res.MaxImages == 0 {
+		res.MaxImages = math.MaxInt32
+	}
+
+	return &res, nil
+}
+
+// Options GET: /options
+func (n Node) Options() ([]OptionResponse, error) {
+	options := []OptionResponse{}
+	body, err := n.apiGET("/options")
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(body, &options); err != nil {
+		return nil, err
+	}
+
+	sort.Slice(options, func(i, j int) bool {
+		return options[i].Name < options[j].Name
+	})
+
+	return options, nil
+}
+
+// TaskInfo GET: /task/<uuid>/info
+func (n Node) TaskInfo(uuid string) (*TaskInfoResponse, error) {
+	res := TaskInfoResponse{}
+	body, err := n.apiGET("/task/" + uuid + "/info")
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
+
+// TaskOutput GET: /task/<uuid>/output
+func (n Node) TaskOutput(uuid string, line int) ([]string, error) {
+	res := []string{}
+	body, err := n.apiGET("/task/" + uuid + "/output?line=" + strconv.Itoa(line))
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+// TaskDownload GET: /task/<uuid>/download/<asset>
+func (n Node) TaskDownload(uuid string, asset string, outputFile string) error {
+	var bar *pb.ProgressBar
+
+	out, err := os.Create(outputFile)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(n.URLFor("/task/" + uuid + "/download/" + asset))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	totalBytes, err := strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 64)
+	if err != nil {
+		totalBytes = 0
+		logger.Debug("Warning: Content-length not set")
+	}
+
+	showProgress := !logger.QuietFlag && totalBytes > 0
+	if showProgress {
+		bar = pb.New64(totalBytes).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 10)
+		bar.Start()
+		bar.Prefix("[" + asset + "]")
+	}
+
+	var writer io.Writer
+	if bar != nil {
+		writer = io.MultiWriter(out, bar)
+	} else {
+		writer = out
+	}
+
+	written, err := io.Copy(writer, resp.Body)
+	if written == 0 {
+		return errors.New("Download returned 0 bytes")
+	}
+
+	if showProgress {
+		bar.Finish()
+	}
+
+	return nil
+}
+
+// TaskCancel POST: /task/cancel
+func (n Node) TaskCancel(uuid string) error {
+	res := ApiActionResponse{}
+
+	body, err := n.apiPOST("/task/cancel", map[string]string{"uuid": uuid})
+	if err != nil {
+		return err
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return err
+	}
+
+	if res.Error != "" {
+		return errors.New(res.Error)
+	}
+
+	return nil
+}
+
+// TaskNewInit POST: /task/new/init
+func (n Node) TaskNewInit(jsonOptions []byte) TaskNewResponse {
+	var err error
+	reqBody := &bytes.Buffer{}
+	mpw := multipart.NewWriter(reqBody)
+	mpw.WriteField("skipPostProcessing", "true")
+	mpw.WriteField("options", string(jsonOptions))
+	if err = mpw.Close(); err != nil {
+		return TaskNewResponse{"", err.Error()}
+	}
+
+	resp, err := http.Post(n.URLFor("/task/new/init"), mpw.FormDataContentType(), reqBody)
+	defer resp.Body.Close()
+	if err != nil {
+		return TaskNewResponse{"", err.Error()}
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return TaskNewResponse{"", err.Error()}
+	}
+
+	var res TaskNewResponse
+	if err := json.Unmarshal(body, &res); err != nil {
+		return TaskNewResponse{"", err.Error()}
+	}
+
+	return res
+}
+
+// TaskNewUpload POST: /task/new/upload/{uuid}
+func (n Node) TaskNewUpload(file string, uuid string, bar *pb.ProgressBar) error {
+	var f *os.File
+	var fi os.FileInfo
+	var err error
+	r, w := io.Pipe()
+	mpw := multipart.NewWriter(w)
+
+	go func() {
+		var part io.Writer
+		defer w.Close()
+		defer f.Close()
+
+		if f, err = os.Open(file); err != nil {
+			return
+		}
+		if fi, err = f.Stat(); err != nil {
+			return
+		}
+
+		if bar != nil {
+			bar.SetTotal64(fi.Size())
+			bar.Set64(0)
+			bar.Prefix("[" + fi.Name() + "]")
+		}
+
+		if part, err = mpw.CreateFormFile("images", fi.Name()); err != nil {
+			return
+		}
+
+		if bar != nil {
+			part = io.MultiWriter(part, bar)
+		}
+
+		if _, err = io.Copy(part, f); err != nil {
+			return
+		}
+
+		if err = mpw.Close(); err != nil {
+			return
+		}
+	}()
+
+	resp, err := http.Post(n.URLFor("/task/new/upload/"+uuid), mpw.FormDataContentType(), r)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var res ApiActionResponse
+	if err := json.Unmarshal(body, &res); err != nil {
+		return err
+	}
+
+	if res.Error != "" {
+		return errors.New(res.Error)
+	}
+
+	if !res.Success {
+		return errors.New("Cannot complete upload. /task/new/upload failed with success: false")
+	}
+
+	return nil
+}
+
+// TaskNewCommit POST: /task/new/commit/{uuid}
+func (n Node) TaskNewCommit(uuid string) TaskNewResponse {
+	var res TaskNewResponse
+
+	body, err := n.apiPOST("/task/new/commit/"+uuid, map[string]string{})
+	if err != nil {
+		return TaskNewResponse{"", err.Error()}
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return TaskNewResponse{"", err.Error()}
+	}
+
+	return res
+}
+
+func (n Node) CheckAuthentication(err error) error {
+	if err != nil {
+		if err == ErrUnauthorized {
+			// Is there a token?
+			if n.Token == "" {
+				return ErrAuthRequired
+			} else {
+				return errors.New("Cannot authenticate with the node (invalid token).")
+			}
+		} else {
+			return err
+		}
+	}
+
+	return nil
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+func (n Node) TryLogin(username string, password string) (token string, err error) {
+	res := AuthInfoResponse{}
+	body, err := n.apiGET("/auth/info")
+	if err != nil {
+		return "", err
+	}
+	if err := json.Unmarshal(body, &res); err != nil {
+		return "", err
+	}
+
+	if res.Message != "" {
+		logger.Info("")
+		logger.Info(res.Message)
+		logger.Info("")
+	}
+
+	if res.LoginUrl != "" {
+		if username == "" && password == "" {
+			username, password = odxio.GetUsernamePassword()
+		}
+
+		logger.Debug("")
+		logger.Debug("POST: " + res.LoginUrl)
+
+		formData, _ := json.Marshal(map[string]string{"username": username, "password": password})
+		resp, err := http.Post(res.LoginUrl, "application/json", bytes.NewBuffer(formData))
+		if err != nil {
+			return "", err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 200 {
+			return "", errors.New("Login URL returned status code: " + strconv.Itoa(resp.StatusCode))
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+
+		res := LoginResponse{}
+		if err := json.Unmarshal(body, &res); err != nil {
+			return "", err
+		}
+
+		if res.Token == "" {
+			return "", errors.New("Login failed")
+		}
+
+		return res.Token, nil
+	}
+
+	// TODO: support for res.RegisterUrl
+
+	return "", errors.New("Cannot login")
+}
---
+++ internal/odx/run.go
@@ -0,0 +1,353 @@
+// Copyright © 2026 CloudODX Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package odx
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"os/signal"
+	"path"
+	"syscall"
+	"time"
+
+	"github.com/WebODM/CloudODX/internal/fs"
+
+	"github.com/WebODM/CloudODX/internal/logger"
+
+	"github.com/cheggaaa/pb"
+)
+
+type fileUpload struct {
+	filename string
+	retries  int
+}
+
+type fileUploadResult struct {
+	filename string
+	err      error
+	retries  int
+}
+
+func singleUpload(node Node, files []string, jsonOptions []byte) TaskNewResponse {
+	var f *os.File
+	var fi os.FileInfo
+	var err error
+	var bar *pb.ProgressBar
+	var res TaskNewResponse
+
+	showProgress := !logger.QuietFlag
+
+	if showProgress {
+		var totalBytes int64
+
+		// Calculate total upload size
+		for _, file := range files {
+			if fi, err = os.Stat(file); err != nil {
+				logger.Error(err)
+			}
+			totalBytes += fi.Size()
+		}
+
+		bar = pb.New64(totalBytes).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 10)
+		bar.Start()
+	}
+
+	r, w := io.Pipe()
+	mpw := multipart.NewWriter(w)
+
+	// Pipe work, stream file contents
+	go func() {
+		var part io.Writer
+		defer w.Close()
+		defer f.Close()
+
+		for _, file := range files {
+			if f, err = os.Open(file); err != nil {
+				logger.Error(err)
+			}
+			if fi, err = f.Stat(); err != nil {
+				logger.Error(err)
+			}
+
+			if part, err = mpw.CreateFormFile("images", fi.Name()); err != nil {
+				logger.Error(err)
+			}
+
+			if showProgress {
+				bar.Prefix("[" + fi.Name() + "]")
+			}
+
+			part = io.MultiWriter(part, bar)
+
+			if _, err = io.Copy(part, f); err != nil {
+				logger.Error(err)
+			}
+		}
+
+		mpw.WriteField("skipPostProcessing", "true")
+		mpw.WriteField("options", string(jsonOptions))
+
+		if err = mpw.Close(); err != nil {
+			logger.Error(err)
+		}
+	}()
+
+	resp, err := http.Post(node.URLFor("/task/new"), mpw.FormDataContentType(), r)
+	if err != nil {
+		logger.Error(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger.Error(err)
+	}
+
+	if showProgress {
+		bar.Finish()
+	}
+
+	if err := json.Unmarshal(body, &res); err != nil {
+		logger.Error(err)
+	}
+
+	return res
+}
+
+func uploadWorker(id int, node Node, uuid string, barPool *pb.Pool, filesToProcess <-chan fileUpload, results chan<- fileUploadResult) {
+	var bar *pb.ProgressBar
+
+	if barPool != nil {
+		bar = pb.New64(0).SetUnits(pb.U_BYTES).SetRefreshRate(time.Millisecond * 10)
+		barPool.Add(bar)
+	}
+
+	for f := range filesToProcess {
+		results <- fileUploadResult{f.filename, node.TaskNewUpload(f.filename, uuid, bar), f.retries}
+	}
+}
+
+func chunkedUpload(node Node, files []string, jsonOptions []byte, parallelUploads int, maxUploadRetries int) TaskNewResponse {
+	var err error
+	var barPool *pb.Pool
+	var mainBar *pb.ProgressBar
+	var res TaskNewResponse
+
+	showProgress := !logger.QuietFlag
+
+	// Invoke /task/new/init
+	res = node.TaskNewInit(jsonOptions)
+	if res.Error != "" {
+		logger.Error(err)
+	}
+
+	if showProgress {
+		barPool = pb.NewPool()
+	}
+
+	// Create workers
+	filesToProcess := make(chan fileUpload, len(files))
+	results := make(chan fileUploadResult, len(files))
+
+	for w := 1; w <= parallelUploads; w++ {
+		go uploadWorker(w, node, res.UUID, barPool, filesToProcess, results)
+	}
+
+	if barPool != nil {
+		barPool.Start()
+
+		mainBar = pb.New(len(files)).SetUnits(pb.U_NO).SetRefreshRate(time.Millisecond * 10)
+		mainBar.Format("[\x00#\x00\x00_\x00]")
+		mainBar.Prefix("Files Uploaded:")
+		mainBar.Start()
+	}
+
+	// Fill queue
+	for _, file := range files {
+		filesToProcess <- fileUpload{file, 0}
+	}
+
+	// Wait
+	filesLeft := len(files)
+	for filesLeft > 0 {
+		fur := <-results
+
+		if fur.err != nil {
+			if fur.retries < maxUploadRetries {
+				// Retry
+				filesToProcess <- fileUpload{fur.filename, fur.retries + 1}
+			} else {
+				logger.Error(errors.New("Cannot upload " + fur.filename + ", exceeded max retries (" + string(maxUploadRetries) + ")"))
+			}
+		} else {
+			filesLeft--
+			if mainBar != nil {
+				mainBar.Set(len(files) - filesLeft)
+			}
+		}
+	}
+	close(filesToProcess)
+
+	if barPool != nil {
+		barPool.Stop()
+	}
+	if mainBar != nil {
+		mainBar.Finish()
+	}
+
+	// Commit
+	res = node.TaskNewCommit(res.UUID)
+	if res.Error != "" {
+		logger.Error(res.Error)
+	}
+
+	return res
+}
+
+// Run processes a dataset
+func Run(files []string, options []Option, node Node, outputPath string, parallelConnections int, maxUploadRetries int) {
+	var err error
+
+	// Convert options to JSON
+	jsonOptions, err := json.Marshal(options)
+	if err != nil {
+		logger.Error(err)
+	}
+
+	var res TaskNewResponse
+	if parallelConnections <= 1 {
+		res = singleUpload(node, files, jsonOptions)
+	} else {
+		res = chunkedUpload(node, files, jsonOptions, parallelConnections, maxUploadRetries)
+	}
+
+	// Handle error response from API
+	if res.Error != "" {
+		logger.Error(res.Error)
+	}
+
+	// We should have a UUID
+	uuid := res.UUID
+	logger.Info("Task UUID: " + uuid)
+
+	info, err := node.TaskInfo(uuid)
+	if err != nil {
+		logger.Error(err)
+	}
+
+	// Catch CTRL+C
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+
+		logger.Info("Canceling task...")
+
+		// Attempt to cancel task
+		retryCount := 0
+		retryLimit := 5
+
+		for retryCount < retryLimit {
+			if err := node.TaskCancel(uuid); err != nil {
+				retryCount++
+				logger.Info(err)
+				time.Sleep(1 * time.Second)
+			} else {
+				break
+			}
+		}
+
+		filesCount, err := fs.DirectoryFilesCount(outputPath)
+		if err == nil && fs.IsDirectory(outputPath) && filesCount == 0 {
+			os.Remove(outputPath)
+		}
+
+		os.Exit(1)
+	}()
+
+	// Start listening for output and task updates...
+	status := info.Status.Code
+	lineNum := 0
+
+	for status == STATUS_QUEUED || status == STATUS_RUNNING {
+		time.Sleep(3 * time.Second)
+
+		info, err := node.TaskInfo(uuid)
+		if err != nil {
+			logger.Info(err)
+
+			// Log error, try again later
+			continue
+		}
+
+		status = info.Status.Code
+
+		lines, err := node.TaskOutput(uuid, lineNum)
+		if err != nil {
+			logger.Info(err)
+			continue
+		}
+
+		for _, line := range lines {
+			logger.Info(line)
+		}
+		lineNum += len(lines)
+	}
+
+	if status == STATUS_CANCELED || status == STATUS_FAILED {
+		os.Exit(1)
+	}
+
+	if status == STATUS_COMPLETED {
+		retryCount := 0
+		retryLimit := 10
+
+		archiveDst := path.Join(outputPath, "all.zip")
+		logger.Info("Task completed! Downloading and extracting results...")
+		logger.Info("")
+
+		for {
+			err := node.TaskDownload(uuid, "all.zip", archiveDst)
+			if err == nil {
+				break
+			} else {
+				logger.Info("Error downloading file (" + err.Error() + ") retrying in " + string(3*retryLimit) + " seconds...")
+				time.Sleep(time.Duration(3*retryLimit) * time.Second)
+				retryCount++
+				if retryCount >= retryLimit {
+					logger.Error("Download retries limit exceeded (" + string(retryLimit) + "), exiting...")
+				}
+			}
+		}
+
+		// Unzip
+		_, err := fs.Unzip(archiveDst, outputPath)
+		if err != nil {
+			logger.Error(err)
+		}
+
+		// Remove
+		if err := os.Remove(archiveDst); err != nil {
+			logger.Info(err)
+		}
+
+		logger.Info("Done! Results saved in " + outputPath)
+	}
+}
